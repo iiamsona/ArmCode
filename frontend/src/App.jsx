9c361ff9bbcad466d621da20fd89c093e4d2da51
iiamsona <sona22hakobyan@gmail.com>
2024-11-09 14:14:22 +0400

    feat: start

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import '@fontsource/roboto/400.css';
+import './bootstrap-grid.min.css';
+import Register from './components/Register';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <div>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/logout" element={<Logout />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
